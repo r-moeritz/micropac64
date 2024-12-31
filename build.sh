85466844a078694f7ba37cb5b5b37707bbf354c2
Ralph Moeritz <426109+rmoritz@users.noreply.github.com>
2024-12-31 15:53:26 +1000

    Create build dir if it doesn't exist

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 name=micropac64
+
+mkdir -p ./build
+
 vasm6502_oldstyle ./src/startup.asm -cbm-prg -Fbin -chklabels -nocase \
                   -o ./build/startup.prg -L "./build/$name.txt" \
     && exomizer sfx sys -t 64 -o "./build/$name.prg" ./build/startup.prg
